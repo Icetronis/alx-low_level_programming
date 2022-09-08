ba1269af181f5f35fd041665fc608ea48f6fc1b5
Emmanuel Ekenechukwu <icetronicsworld@gmail.com>
2022-09-08 13:08:34 -0700

    A rogram that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 
-
 /**
  *
  *  * main - Entry point
@@ -22,6 +21,7 @@ int main(void)
 
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+
+return (0);
 
 }
