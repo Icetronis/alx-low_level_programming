5674c11b8e38172d2936728b7e8bf592490f0657
Emmanuel Ekenechukwu <icetronicsworld@gmail.com>
2022-09-13 14:53:23 -0700

    Identifying the lowecase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+_islower - function checks lowercasae character
+*@c: the int that will be used for the arguement of the function
+*Return: 0
+*/
+int_islower(int c)
+{
+if (c >= 'a' && c <== 'z')
+{
+return (1);
+}
+else
+return (0);
+}
