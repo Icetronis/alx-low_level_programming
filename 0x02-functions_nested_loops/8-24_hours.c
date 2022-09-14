7b0a1ab4b9ae46e3083db5d22684fb4d5755d712
Emmanuel Ekenechukwu <icetronicsworld@gmail.com>
2022-09-14 00:00:11 -0700

    Update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*jack_bauer - function that prints evry minute of the day of Jack Baue*r, starting from 00:00 to 23:59
+*jack_bauer - function that prints evry minute of the day from 00:00 to 23:59
 *Return: 0
 */
 void jack_bauer(void)
