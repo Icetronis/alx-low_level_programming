0dce6e433a9d63316d14a51fa9f819ca9f358e90
Emmanuel Ekenechukwu <icetronicsworld@gmail.com>
2022-09-16 14:06:34 -0700

    UpdATE

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,29 +21,28 @@ int main(void)
 
 		else if (i % 3 != 0 && i % 5 == 0)
 
+		{
 			printf("Buzz");
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0)
+
+		{
+			printf("FizzBuzz");
+		}
+
+		else
+
+		{
+			printf("%d", i);
+		}
+
+		if (i != 100)
+			putchar(' ');
+		else
+			putchar('\n');
 	}
 
-	else if (i % 3 == 0 && i % 5 == 0)
-
-	{
-		printf("FizzBuzz");
-	}
-
-	else
-
-	{
-		printf("%d", i);
-	}
-
-	if (i != 100)
-
-		putchar(' ');
-
-	else
-		putchar('\n');
-
-
-return (0);
+	return (0);
 
 }
